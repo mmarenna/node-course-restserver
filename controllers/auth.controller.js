889d8ad7414c337f7edc6bd131700e202027488f
Matias Marenna <matimarenna@gmail.com>
2022-06-25 17:04:57 -0300

    push google signin

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,8 +1,10 @@
 const { response } = require("express");
 const bcryptjs = require('bcryptjs');
 
-const Usuario = require('../models/user');
+
 const { generateJWT } = require("../helpers/jwt-generator");
+const { googleVerify } = require("../helpers/google-validation");
+const User = require("../models/user");
 
 const login = async(req, res = response) => {
 
@@ -10,7 +12,7 @@ const login = async(req, res = response) => {
 
     try {
         //Verificar si el email existe
-        const user = await Usuario.findOne({email}); 
+        const user = await User.findOne({email}); 
   
         //Verificar si esta activo
         if(!user || user.status === false) {
@@ -37,14 +39,57 @@ const login = async(req, res = response) => {
         })
     }
 
-    function badRequestFormaterResponse( message ) {
-        res.status(400).json({
-            msg: message
-        });
-    }
+    
+
 }
+
+const googleSignIn = async(req, res = response) => {
+
+    const { id_token} = req.body;
+
+    try {
+        const {name, picture, email} = await googleVerify(id_token);
+        
+        let usuario = await User.findOne({ email });
+        
+        if (!await User.findOne({ email })) {
+            // Tengo que crearlo
+            const data = {
+                name,
+                picture,
+                email,
+                password: ':P',
+                google: true
+            };
+            usuario = new User( data );
+            console.log(usuario);
+            // await usuario.save();
+        }
+
+        
+        if(!usuario.status) {
+            res.status(401).json({
+                msg: 'User not authorized!'
+            })
+        }
+
+        console.log(usuario.id);
+        const jwtToken = await generateJWT(usuario.id);
+        
+        res.json({
+            usuario,
+            jwtToken
+        })
+    } catch (error) {
+        res.status(400).json({
+            ok: false,
+            msg: 'El token no se pudo verificar'
+        })
+    }
+} 
 
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
